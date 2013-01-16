134c31ea305a2f5b243516b03b90719c413f8ca7
Long Ho <holevietlong@gmail.com>
2013-01-16 11:44:25 -0500

    indentation bs

--- src/Heap.js
+++ src/Heap.js
@@ -1,91 +1,91 @@
 (function () {
 
-	var Utils = this.Utils || require('./Utils');
+  var Utils = this.Utils || require('./Utils');
 
-	var Heap = function (heap) {
-		var _nodes
-			, self = this;
+  var Heap = function (heap) {
+    var _nodes
+      , self = this;
 
-		//Initialization
-		var _init = function () {
-			_nodes = [];			
-			if (heap) {
-				self.insertAll(heap);
-			}
-		};
+    //Initialization
+    var _init = function () {
+      _nodes = [];      
+      if (heap) {
+        self.insertAll(heap);
+      }
+    };
 
-		//Insert all elements in `nodes` into the current heap.
-		//Nodes can be a Heap, an array or an object.
-		this.insertAll = function (nodes) {
-			//If nodes is already a Heap, great
-			if (nodes instanceof Heap) {
-				var keys = nodes.keys()
-					, values = nodes.values();
-				//Short-circuit this since we have no elements
-				if (self.size() === 0) {
-					for (var i = 0; i < keys.length; i++) {
-						_nodes.push({ key: keys[i], value: values[i] });
-					}
-				}
-			} else if (Utils.isArray(nodes)) {	// Array of node objects
-				for (var i = 0; i < nodes.length; i++) {
-					self.insert(nodes[i].key, nodes[i].value);
-				}
-			} else {
-				for (var k in nodes) {	//Object with key-value
-					self.insert(k, nodes[k]);
-				}
-			}
-		};
+    //Insert all elements in `nodes` into the current heap.
+    //Nodes can be a Heap, an array or an object.
+    this.insertAll = function (nodes) {
+      //If nodes is already a Heap, great
+      if (nodes instanceof Heap) {
+        var keys = nodes.keys()
+          , values = nodes.values();
+        //Short-circuit this since we have no elements
+        if (self.size() === 0) {
+          for (var i = 0; i < keys.length; i++) {
+            _nodes.push({ key: keys[i], value: values[i] });
+          }
+        }
+      } else if (Utils.isArray(nodes)) {  // Array of node objects
+        for (var i = 0; i < nodes.length; i++) {
+          self.insert(nodes[i].key, nodes[i].value);
+        }
+      } else {
+        for (var k in nodes) {  //Object with key-value
+          self.insert(k, nodes[k]);
+        }
+      }
+    };
 
-		this.keys = function () {
-			var keys = [];
-			for (var i = 0; i < _nodes.length; i++) {
-				keys.push(_nodes[i].key);
-			}
-			return keys;
-		};
+    this.keys = function () {
+      var keys = [];
+      for (var i = 0; i < _nodes.length; i++) {
+        keys.push(_nodes[i].key);
+      }
+      return keys;
+    };
 
-		this.values = function () {
-			var vals = [];
-			for (var i = 0; i < _nodes.length; i++) {
-				vals.push(_nodes[i].value);
-			}
-			return vals;
-		};
+    this.values = function () {
+      var vals = [];
+      for (var i = 0; i < _nodes.length; i++) {
+        vals.push(_nodes[i].value);
+      }
+      return vals;
+    };
 
-		this.peek = function () {
-			return _nodes.length ? _nodes[0].value : undefined;
-		};
+    this.peek = function () {
+      return _nodes.length ? _nodes[0].value : undefined;
+    };
 
-		this.size = function () {
-			return _nodes.length;
-		};
+    this.size = function () {
+      return _nodes.length;
+    };
 
-		//Insert a key, value into the heap.
-		this.insert = function (k, v) {
-			if (k === undefined || k === null) throw new Error('undefined or null key is not allowed');
-			var node = { key: k, value: v };
-			_nodes.push(node);
+    //Insert a key, value into the heap.
+    this.insert = function (k, v) {
+      if (k === undefined || k === null) throw new Error('undefined or null key is not allowed');
+      var node = { key: k, value: v };
+      _nodes.push(node);
 
-			for (var i = _nodes.length - 1; i > 0; ) {
-				var parentIndex = (i - 1) >> 1;
-				if (_nodes[parentIndex].key <= k) break;
-				_nodes[i] = _nodes[parentIndex];
-				i = parentIndex;
-			}
-			_nodes[i] = node;
-		};
+      for (var i = _nodes.length - 1; i > 0; ) {
+        var parentIndex = (i - 1) >> 1;
+        if (_nodes[parentIndex].key <= k) break;
+        _nodes[i] = _nodes[parentIndex];
+        i = parentIndex;
+      }
+      _nodes[i] = node;
+    };
 
-		_init();
-	}
+    _init();
+  }
 
-	if (module && module.exports) {
-		module.exports = Heap;
-	} else if (exports) {
-		exports = Heap;
-	} else {
-		this.Heap = Heap
-	}
+  if (module && module.exports) {
+    module.exports = Heap;
+  } else if (exports) {
+    exports = Heap;
+  } else {
+    this.Heap = Heap
+  }
 
 })();
--- test/Heap.test.js
+++ test/Heap.test.js
@@ -1,45 +1,45 @@
 var assert = require("assert")
-	, Heap = require('../src/Heap');
+, Heap = require('../src/Heap');
 describe('Heap', function () {
   describe('#peek()', function () {
-  	it('should return undefined when heap is empty', function () {
-    	assert.equal(undefined, new Heap().peek());
-    });
+    it('should return undefined when heap is empty', function () {
+     assert.equal(undefined, new Heap().peek());
+   });
     it('should return correct value when heap has elements 100 and 1', function () {
-    	var heap = new Heap();
-    	heap.insert(100, 1);
-    	heap.insert(1, 5);
-    	assert.equal(5, heap.peek());
+      var heap = new Heap();
+      heap.insert(100, 1);
+      heap.insert(1, 5);
+      assert.equal(5, heap.peek());
     });
     it('should return correct value when heap has elements 10, 12, 15, 100', function () {
-    	var heap = new Heap();
-    	heap.insert(100, 1);
-    	heap.insert(10, 'top');
-    	heap.insert(15, 88);
-    	heap.insert(12, 'test');
-    	assert.equal('top', heap.peek());
+      var heap = new Heap();
+      heap.insert(100, 1);
+      heap.insert(10, 'top');
+      heap.insert(15, 88);
+      heap.insert(12, 'test');
+      assert.equal('top', heap.peek());
     });
   });
   describe('#size()', function () {
     it('should return 0 when heap is empty', function () {
-        assert.equal(0, new Heap().size());
+      assert.equal(0, new Heap().size());
     });
     it('should return 2 when heap has 2 elements', function () {
-        var heap = new Heap();
-        heap.insert(100, 1);
-        heap.insert(1, 5);
-        assert.equal(2, heap.size());
+      var heap = new Heap();
+      heap.insert(100, 1);
+      heap.insert(1, 5);
+      assert.equal(2, heap.size());
     });
   });
   describe('#insert()', function () {
     it('should throw error when trying to insert null or undefined keys', function () {
-        var heap = new Heap();
-        assert.throws(function () {
-            heap.insert(null, 0);
-        }, Error);
-        assert.throws(function () {
-            heap.insert(undefined, 0);
-        }, Error);
+      var heap = new Heap();
+      assert.throws(function () {
+        heap.insert(null, 0);
+      }, Error);
+      assert.throws(function () {
+        heap.insert(undefined, 0);
+      }, Error);
     });
   });
   describe('#insertAll()', function () {
@@ -49,15 +49,15 @@ describe('Heap', function () {
     });
     it('should be able to insertAll an array of nodes', function () {
       var heap = new Heap([
-          { key: 1, value: 1 },
-          { key: 2, value: 2 }
+        { key: 1, value: 1 },
+        { key: 2, value: 2 }
         ])
       assert.equal(2, heap.size());
     });
     it('should be able to insertAll another Heap', function () {
       var heap = new Heap([
-          { key: 1, value: 1 },
-          { key: 2, value: 2 }
+        { key: 1, value: 1 },
+        { key: 2, value: 2 }
         ])
       assert.equal(2, new Heap(heap).size());
     });
